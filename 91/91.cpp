9dd6c1894750da8ab30d6e7edde75419e60202e6
AleksandrSol <66996293+AleksandrSol@users.noreply.github.com>
2020-07-23 02:55:34 +0300

    Добавьте файлы проекта.

---
+++ 91/91.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+        setlocale(LC_CTYPE, "ukr");
+        cout << "++++++++++++++++++++++++++++++++++++\n";
+        cout << "            Iнформацiя:           \n";
+        cout << "Група: IПЗ-I 18к                  \n";
+        cout << "ПІБ: Соляной Александр Олегович\n";
+        cout << "Номер теми: 1.3.1 \n";
+        cout << "Номер завдання: 91 " << "\n";
+        cout << "+++++++++++++++++++++++++++++++++++\n\n";
+
+
+    int b[5][5];
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++) {
+            cout << "Vvedite matrici: ";
+            cin >> b[i][j];
+        }
+
+    int success = 0;
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (i != j)
+                if (b[i][j] == b[j][i])
+                    success = 1;
+                else
+                    success = 0;
+
+    if (success)
+        cout << "Kvadrat simetrichen \n";
+    else
+        cout << "Kvadrat ne simetrichen \n";
+
+}
